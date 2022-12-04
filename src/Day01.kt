2ab558c07c82a99921f5d0444a3ad13bf3e63038
Rishi Kumar <24rkumar@student.dasd.org>
2022-12-04 12:50:07 -0500

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun List<String>.readWithNulls() = scan<_, Int?>(null) { acc, s ->
+        if (s.isBlank()) null
+        else (acc ?: 0) + s.toInt()
+    }.let { it + null }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = input.readWithNulls().filterNotNull().max()
+
+    fun part2(input: List<String>) = input.readWithNulls().let { l ->
+        l.indices.filter { l[it] == null }.map { it - 1 } to l
+    }.let { (i, l) ->
+        l.filterIndexed { index, _ -> i.contains(index) }.filterNotNull()
+    }.sortedDescending().subList(0, 3).sum()
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
